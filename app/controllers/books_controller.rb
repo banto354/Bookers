46bb651d57ad79c90f124f14213f836572d34487
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-04 01:24:48 +0000

    [Edit] fixed index.html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
-    def new
-        @book = Book.new
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to book_path(@book.id)
+        else
+            redirect_to :index
+        end
     end
     
     def index
-        @books.Book.all
+        @book = Book.new
+        @books = Book.all
     end
     
     def show
-        @books.Book.all
+        @book = Book.find(params[:id])
+    end
+    
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title,:body)
     end
 end
